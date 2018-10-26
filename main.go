43cfffae648a3a565513c7551d15c870ce6e1160
Chris Price <chrisprice@rogers.com>
2018-10-26 15:17:42 -0400

    Use new signal handler

--- main.go
+++ main.go
@@ -3,58 +3,19 @@ package main
 import (
 	"context"
 	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
 
-	"github.com/google/uuid"
 	"github.com/pricec/golib/kafka"
 	"github.com/pricec/golib/log"
+	"github.com/pricec/golib/signal"
 	"github.com/pricec/gateway/session"
 )
-
-func sigHandler(
-	ctx context.Context,
-	wantExit *bool,
-	doneChan chan<- struct{},
-	sigChan <-chan os.Signal,
-) {
-	go func() {
-		for {
-			select{
-			case sig := <-sigChan:
-				switch sig {
-				case syscall.SIGINT:
-					fallthrough
-				case syscall.SIGTERM:
-					*wantExit = true
-					fallthrough
-				case syscall.SIGHUP:
-					doneChan <- struct{}{}
-				default:
-					log.Warning("Received unrecognized signal %v", sig)
-				}
-			case <- ctx.Done():
-				return
-			}
-		}
-	}()
-}
-
-type AckMessage struct {
-	Id      string `json:"id"`
-	Message []byte `json:"message"`
-}
 
 func ReadCb(
 	km *kafka.KafkaManager,
 ) func(*session.SessionManager, session.SessionId, []byte) {
 	return func(s *session.SessionManager, id session.SessionId, data []byte) {
 		log.Debug("Received message from %v: %+v", id, data)
-		ack := AckMessage{ Id: uuid.New().String(), Message: data }
-		if err := s.Write(id, ack); err != nil {
-			log.Warning("Failed to ack message '%v': %v", data, err)
-		} else if err := km.Send("test", data); err != nil {
+		if err := km.Send("test", data); err != nil {
 			log.Err("Failed to send message '%v' to kafka: %v", data, err)
 			// TODO: send a message indicating the failure
 		}
@@ -68,15 +29,11 @@ func main() {
 	log.Info("Starting gateway...")
 
 	// Variable declarations
-	wantExit := false
 	ready := false
 	ctx, cancel := context.WithCancel(context.Background())
-	sigChan := make (chan os.Signal)
-	doneChan := make(chan struct{})
 
-	// Signal handling
-	sigHandler(ctx, &wantExit, doneChan, sigChan)
-	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
+	// Initialize signal handling
+	done, reload := signal.Setup(ctx)
 
 	// Initialize the Kafka manager
 	km, err := kafka.NewKafkaManager(ctx, "kafka.common", uint16(9092))
@@ -103,10 +60,10 @@ func main() {
 		}
 	}()
 
-	for !wantExit {
+	for !done() {
 		log.Info("Initializing gateway")
 		ready = true
-		<- doneChan
+		<-reload()
 		ready = false
 	}
 	cancel()
